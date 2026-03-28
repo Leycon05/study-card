731bcb773eb53985b1c49f2846621ed7cd1beca6
leycon05 <limaleycon@gmail.com>
2026-03-28 18:01:53 -0300

    iniciando a tela home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Cadastro from './pages/Cadastro.jsx';
 import Login from './pages/Login.jsx';
+import Home from "./pages/Home";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         {/* Define os caminhos de cada tela */}
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,87 @@
+import { FiLogOut } from "react-icons/fi";
+import "../styles/home.css";
+        <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet"></link>
+
+function Home() {
+  const nome = "Leycon";
+
+  return (
+    <div className="container">
+      {/* HEADER */}
+    <header className="header">
+  <div className="header-content">
+    <h2>Study card</h2>
+
+    <button className="logout">
+      <FiLogOut size={24} />
+    </button>
+  </div>
+</header>
+
+      {/* BOAS VINDAS */}
+      <div className="welcome">
+        <div className="welcome-box">
+          <h1>Bem-Vindo,</h1>
+        </div>
+
+        <div className="name-box">
+          <h2>({nome})</h2>
+        </div>
+
+        <button className="btn-novo">novo +</button>
+      </div>
+
+      {/* TAREFAS */}
+      <h3 className="titulo">minhas tarefas</h3>
+
+      <div className="cards">
+        {/* CARD 1 */}
+        <div className="card">
+          <div className="card-top">
+            <strong>Nome</strong>
+            <span>Tema</span>
+          </div>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+
+          <div className="card-info">
+            <span>⏱ inicio - fim</span>
+            <span>🔷 pendente</span>
+          </div>
+
+          <div className="card-buttons">
+            <button className="cancelar">Cancelar</button>
+            <button className="concluir">Concluir</button>
+          </div>
+        </div>
+
+        {/* CARD 2 */}
+        <div className="card">
+          <div className="card-top">
+            <strong>Nome</strong>
+            <span>Tema</span>
+          </div>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+
+          <div className="card-info">
+            <span>⏱ inicio - fim</span>
+            <span>✔ concluído</span>
+          </div>
+
+          <div className="card-buttons">
+            <button className="excluir">Excluir</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
